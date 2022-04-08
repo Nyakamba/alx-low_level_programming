b65059f6d8ccdb24c548157b31bcd1506f507f34
Nyakamba <omwegaenock@gmail.com>
2022-04-08 13:08:57 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,22 @@
 int main(void)
 
 {
-	char lower_case;
-	char upper_case;
+char lower_case;
+char upper_case;
 
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-	{
-		putchar(upper_case);
-	}
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);
+}
 
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-        {
-	        putchar(lower_case);
-        }
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+{
+putchar(lower_case)
+}
 
-	put('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 }
 
 
