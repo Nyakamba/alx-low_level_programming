f05d156cbc65094751eb2a9144722705a1028302
Nyakamba <omwegaenock@gmail.com>
2022-04-08 03:59:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  *main -entry point
+ *
+ *Returnr 0 after the function
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); 
-	return  (0);
+puts("\"Programming is like building a multilingual puzzle"); 
+return  (0);
 }
