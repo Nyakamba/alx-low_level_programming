68f21101896fdc5be66a877a9b59e98a61aef613
Nyakamba <omwegaenock@gmail.com>
2022-04-08 03:53:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  *main -entry point
- *return o after printing function
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle"); 
+	return  (0);
 }
