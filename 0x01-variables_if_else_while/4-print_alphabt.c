2f1d849b7015ea127ae688696be5f47e04b2cdf8
Nyakamba <omwegaenock@gmail.com>
2022-04-09 11:01:28 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alpahbet == 'q')
+			continue;
+
+		else if (alphabet == 'e')
+			continue;
+		putchar(alpahbet);
+	}
+
+	putchar('\n')
+
+		return (0);
+}
