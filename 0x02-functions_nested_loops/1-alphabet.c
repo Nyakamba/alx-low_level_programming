fa1d7801a348c36a533d4dba028b57a1731e5daf
Nyakamba <omwegaenock@gmail.com>
2022-04-12 08:25:48 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,15 @@
  * Return: Always 0.
  */
 
-void print_alphabet(void)
+void print_alphabet(void);
 {
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-_putchar(alphabet);
+putchar(alphabet);
 }
 
-_putchar('\n');
+putchar('\n');
 
 }
