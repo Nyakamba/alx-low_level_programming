e229555ac315b5b7089cda37694e0f2788eef9ad
Nyakamba <omwegaenock@gmail.com>
2022-04-12 08:47:14 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,15 @@
  * Return: Always 0.
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-putchar(alphabet);
+_putchar(alphabet);
 }
 
-putchar('\n');
+_putchar('\n');
 
 }
