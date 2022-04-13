19ca94bf2f01d52045480d3a583114ba5694c579
Nyakamba <omwegaenock@gmail.com>
2022-04-13 09:51:59 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-	int i, sum = 0;
-	for (i = 0; i < 1024; i++)
-	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-				sum += i;
-				}
-	
-				printf("%d\n", sum);
+int i, sum = 0;
 
-				return (0);
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+sum += i;
+}
 
-				}
+printf("%d\n", sum);
+
+return (0);
+
+}
